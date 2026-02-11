3bab1519cf9ab90934b14ceab695e4f9c54b9471
Abhay-Pratap-Singh-7 <abhaypratapsingh2993@gmail.com>
2026-02-11 23:53:55 +0530

    Fixed CORS error

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import requests
 import json
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": ["https://your-site.netlify.app", "http://localhost:3000"]}})
+CORS(app, resources={r"/api/*": {"origins": ["https://trackbook-erp.netlify.app", "http://localhost:3000"]}})
 
 BASE_URL = "https://erp.akgec.ac.in"
 
